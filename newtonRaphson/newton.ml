47beb2cf02cd4e38202f8e31ac49b9708dabced9
Saumil Ambani <saumilambani@users.noreply.github.com>
2017-08-04 00:20:26 -0400

    Removed redundant argument from function

--- newtonRaphson/newton.ml
+++ newtonRaphson/newton.ml
@@ -3,20 +3,20 @@
 let eps = 0.001 in
 
 let rec newton_raphson x a =  
-   let improv x a = (x +. a/.x)/.2. in 
+   let improv x = (x +. a/.x)/.2. in 
 
-   let satis x a = 
+   let satis x = 
       if (abs_float(x*.x -. a) < eps) then 
          true
       else 
          false
    in
 
-   if satis x a then
+   if satis x then
       let () = Printf.printf "Sq %f = %f\n" a x in
       x
    else 
-      newton_raphson (improv x a) a 
+      newton_raphson (improv x) a 
 in
 
 (* Assuming initial guess to be the same as the number *)
