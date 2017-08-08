b1e0d3ad7460737309a7b4778c7a21d71a3aade0
Saumil Ambani <saumilambani@users.noreply.github.com>
2017-08-08 08:28:56 -0400

    Tail recursion

---
+++ tailRecursion/rec.ml
@@ -0,0 +1,26 @@
+(* Calculating factorial using tail and non-tail recursion *)
+
+(* Tail Recursion *)
+let rec fact i =
+   if i = 0 then
+      1
+   else
+      i*(fact (i - 1))
+in
+   
+Printf.printf "%n \n" (fact 5);;
+
+
+(* Non tail recursion *)
+let fact2 i = 
+  let rec loop acc i = 
+    if i = 0 then
+      acc
+   else
+      loop (acc*i) (i-1)
+  in
+  loop 1
+in
+
+let k = fact2 5 in
+Printf.printf "%n \n" k;;
