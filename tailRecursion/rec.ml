8922b2f2b1987d9a1c26574124aa0bd201e3dca9
Saumil Ambani <saumilambani@users.noreply.github.com>
2017-08-13 21:27:26 -0400

    Formating and cleanup

--- tailRecursion/rec.ml
+++ tailRecursion/rec.ml
@@ -1,18 +1,17 @@
 (* Calculating factorial using tail and non-tail recursion *)
 
 (*** FACTORIAL ***)
-(* Tail Recursion *)
+(* NON TAIL RECURSION *)
 let rec fact i =
    if i = 0 then
       1
    else
       i*(fact (i - 1))
 in
-   
 Printf.printf "%n \n" (fact 5);;
 
 
-(* Non tail recursion *)
+(* TAIL RECURSION *)
 let fact2 i = 
   let rec loop acc i = 
     if i = 0 then
@@ -22,15 +21,25 @@ let fact2 i =
   in
   loop 1 i
 in
-
 let k = fact2 5 in
 Printf.printf "%n \n" k;;
 
-(** REVERSING A LIST **)
+(** Reversing a list(l) **)
+let l = [1;2;3;4];;
+
 let rec rev acc = function
-  [] -> acc 
+| [] -> acc 
 | h::t -> rev (h::acc) t
 
-
-let a = rev [] [1;2;3;4];; 
+let a = rev [] l;; 
 let () = List.iter (Printf.printf "%n ") a;;
+
+(** Applying a function(f) to a list(l) **)
+let f a  = 2*a;;
+
+let rec rev_map f acc = function
+| [] -> acc 
+| h::t -> rev_map f (f h::acc) t
+
+let l_f = rev [] (rev_map f [] l);; 
+let () = List.iter (Printf.printf "%n ") l_f;;
